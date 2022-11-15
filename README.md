eefd5182439f369ba22d03c4d08905f0c4e660de
Sreenivas s pai <111109226+sreeni28081994@users.noreply.github.com>
2022-11-16 00:55:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# File Upload with image showing
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Anon_12/pen/xxrwqGP](https://codepen.io/Anon_12/pen/xxrwqGP).
+
---
+++ script.js
@@ -0,0 +1,13 @@
+function preview_images() {
+  var total_file = document.getElementById("images").files.length;
+  for(var i=0;i<total_file;i++){
+    $('#image_preview').append(`
+                <div class='col-md-3'>
+                    <img style='width:100%' class='img-responsive' src='${URL.createObjectURL(event.target.files[i])}'>
+                </div>`);
+  }
+}
+function resetForm(){
+  $("#image_preview").html("");
+  return true;
+}
